025bf69a919cd1fe95910fe2afdd9c2feac6314f
Akosile Tayo <akosilety9@gmail.com>
2021-01-17 00:31:41 +0100

    Did some prettry Javascript here

--- script/main.js
+++ script/main.js
@@ -12,28 +12,54 @@ let game = (function () {
         rockPaperAndScissor = document.querySelectorAll('.main__games img');
 
 
-
     rulesButton.addEventListener('click', openRule)// Open Rules Image
-
     closeImgButton.addEventListener('click', closeRule) //close RUles Image
 
     rockPaperAndScissor.forEach(img => (img.addEventListener('click', showStepTwo)))
 
+
     function openRule() {
         rulesImg.classList.add('active')
     }
-    
+
     function closeRule() {
         rulesImg.classList.remove('active')
 
     }
 
 
+    function randomImageColors() {
+
+    }
     function showStepTwo() {
         let imageClicked = this;
         gameStepOne.classList.remove('active');
         gameStepTwo.classList.add('active');
-        humanPicked.src = imageClicked.src; //Human Picked Answer === imagePicked answer
-        humanPicked.parentNode.classList.add(imageClicked.parentNode.classList.value)
+
+        let changeRandomImages = setInterval(() => {
+            const randomImg = [...rockPaperAndScissor].map(img => img.src);
+            const rnd = Math.floor(Math.random() * randomImg.length)
+            computerPicked.src = randomImg[rnd];
+        }, 350);
+        console.log(computerPicked);
+
+        randomImageColors()
+        humanAnswer();
+        computerAnswer()
+
+        function humanAnswer() {
+            humanPicked.src = imageClicked.src; //Human Picked Answer === imagePicked answer
+            humanPicked.parentNode.classList.add(imageClicked.parentNode.classList.value)
+        }
+
+        function computerAnswer() {
+            const randomImg = [...rockPaperAndScissor].map(img => img.src);
+            const rnd = Math.floor(Math.random() * randomImg.length)
+            setTimeout(() => {
+                clearInterval(changeRandomImages)
+                computerPicked.src = randomImg[rnd];
+            }, 4500);
+
+        }
     }
 })();
