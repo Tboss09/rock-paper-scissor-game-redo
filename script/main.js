1b9e88fc0a6a2e37067002cc7cfd4bb32f5096fd
Akosile Tayo <akosilety9@gmail.com>
2021-01-17 18:22:16 +0100

    Merged a branch

--- script/main.js
+++ script/main.js
@@ -2,8 +2,8 @@ let game = (function () {
     const gameStepOne = document.querySelector('.main'),
         gameStepTwo = document.querySelector('.mainTwo'),
         nextStep = gameStepTwo.querySelectorAll('.circle img'),
-        [humanPicked, computerPicked] = nextStep,
-
+        [humanPicked,] = nextStep,
+        computerPicked = document.querySelector('.theHouseAnswer'),
         // Footer 
         rulesButton = document.querySelector('.footer__btn button'),
         rulesImg = document.querySelector('.footer__img'),
@@ -13,24 +13,25 @@ let game = (function () {
         winOrLose = document.querySelector('.mainTwo__game--winOrLose'),
         winOrLoseAnswer = winOrLose.querySelector('p'),
 
-        //scoreIndicator
 
         // Play Again
-        playAgainButton = document.querySelector(".mainTwo__game--winOrLose")
+        playAgainButton = document.querySelector(".mainTwo__game--winOrLose"),
 
-    // rock paper scissors img
-    rockPaperAndScissor = document.querySelectorAll('.main__games img');
+        // rock paper scissors img
+        rockPaperAndScissor = document.querySelectorAll('.main__games img');
 
+    let lastImagePicked;
 
+    //scoreIndicator
     let scoreIndicator = document.querySelectorAll('.header__score p'),
         [, pTwo] = scoreIndicator,
         score = 0;
     pTwo.textContent = score;
+
     // Opens Rule Image
     rulesButton.addEventListener('click', openRule)// Open Rules Image
     closeImgButton.addEventListener('click', closeRule) //close RUles Image
     playAgainButton.addEventListener("click", replayGame)
-
     rockPaperAndScissor.forEach(img => (img.addEventListener('click', showStepTwo)))
 
     function replayGame() {
@@ -48,23 +49,15 @@ let game = (function () {
         rulesImg.classList.remove('active')
 
     }
-    function restartGame() {
-        gameStepOne.classList.add('active');
-        gameStepTwo.classList.remove('active');
-    }
+
+
+
     function showStepTwo() {
         let imageClicked = this;
         gameStepOne.classList.remove('active');
         gameStepTwo.classList.add('active');
-        let lastImage;
-
-
-        // randomImageColors()
         humanAnswer();
-        computerAnswer()
-
-        // getResult();
-
+        computerAnswer();
         function humanAnswer() {
             // Shows what the user picked
             humanPicked.src = imageClicked.src; //Human Picked Answer === imagePicked answer
@@ -74,89 +67,81 @@ let game = (function () {
 
         function computerAnswer() {
 
-            function getRandomIng() {
-                const randomImg = [...rockPaperAndScissor].map(img => img.src);
-                const rnd = Math.floor(Math.random() * randomImg.length)
-                if (lastImage === randomImg[rnd]) {
-                    console.log("Same Number Pls")
-                    return getRandomIng();
-                }    
-                
-                lastImage = randomImg[rnd];
-                return randomImg[rnd];
-            }
 
-            setTimeout(() => { //show the image after 2 secs
-                computerPicked.src = getRandomIng();
-
-                if (humanAnswer() === computerPicked.src) {
-                    winOrLose.classList.add('active');
-                    winOrLoseAnswer.textContent = " A draw"
-                    return;
-                }
-
-                // Paper
-                else if (humanAnswer() === "http://127.0.0.1:5500/images/icon-paper.svg" && computerPicked.src === "http://127.0.0.1:5500/images/icon-rock.svg") {
-                    winOrLose.classList.add('active');
-                    winOrLoseAnswer.textContent = " You WIn"
-                    score++;
-                    pTwo.textContent = score;
-
-                    console.log(score)
-                    return;
-                }
-
-                // Scissors
-                else if (humanAnswer() === "http://127.0.0.1:5500/images/icon-rock.svg" && computerPicked.src === "http://127.0.0.1:5500/images/icon-scissors.svg") {
-                    winOrLose.classList.add('active');
-                    winOrLoseAnswer.textContent = " You Win"
-                    score++;
-                    pTwo.textContent = score;
-                    return;
-                }
-                // Scissors
-                else if (humanAnswer() === "http://127.0.0.1:5500/images/icon-scissors.svg" && computerPicked.src === "http://127.0.0.1:5500/images/icon-paper.svg") {
-                    winOrLose.classList.add('active');
-                    winOrLoseAnswer.textContent = " You Win"
-                    score++;
-                    pTwo.textContent = score;
-                    return;
-                }
-                else {
-                    winOrLose.classList.add('active');
-                    winOrLoseAnswer.textContent = " You Lose"
-                    score--;
-                    pTwo.textContent = score;
-                    return;
-
-                }
-            }, 2000);
-
+            let interval = setInterval(() => {
+                computerPicked.innerHTML = randomImg();
+            }, 200);
 
             setTimeout(() => {
-                console.log(humanAnswer());
-                console.log(computerPicked.src);
+                clearInterval(interval);
+                computerPicked.innerHTML = randomImg();
+                const game = {
+                    scissor: "http://127.0.0.1:5500/images/icon-scissors.svg",
+                    rock: "http://127.0.0.1:5500/images/icon-rock.svg",
+                    paper: "http://127.0.0.1:5500/images/icon-paper.svg"
+                }
+                const computerImg = computerPicked.querySelector('img').src;
+                console.log(computerPicked.querySelector('img').src)
 
-            }, 5000);
-            // return computerPicked.src;
+                // if (humanAnswer() === game.rock && computerImg === game.scissor) {
+                //     winOrLose.classList.add('active');
+                //     winOrLoseAnswer.textContent = "You Won"
+                //     console.log('same');
+                // }
+                if (humanAnswer() === game.rock && computerImg == game.scissor 
+                ||humanAnswer() === game.paper && computerImg == game.rock 
+                ||humanAnswer() === game.scissor && computerImg == game.paper 
+                ) {
+                    winOrLose.classList.add('active');
+                    winOrLoseAnswer.textContent = "You Won"
+                    score++;
+                    pTwo.textContent = score;
+                }
+
+                else if (humanAnswer() === computerImg) {
+                    winOrLose.classList.add('active');
+                    winOrLoseAnswer.textContent = "A Draw"
+                }
+
+                else {
+                    winOrLose.classList.add('active');
+                    winOrLoseAnswer.textContent = "You Lose"
+                    score--;
+                    pTwo.textContent = score;
+                }
+            }, 2000);
         }
 
+        function randomImg() {
+            let computerImage = [
+                `  <div class="mainTwo__game--computer pulse main__games--hand  circle ">
+             <img src="images/icon-paper.svg" alt="icon-paper">
+<p>The House Picked</p>
+</div>`,
 
-        // function getResult() {
-        //     setTimeout(() => {
+                `  <div class="mainTwo__game--computer pulse main__games--rock  circle ">
+<img src="images/icon-rock.svg" alt="icon-paper">
+<p>The House Picked</p>
+</div>`,
 
-        //         // if (humanAnswer() === "http://127.0.0.1:5500/images/icon-scissors.svg") {
+                `  <div class="mainTwo__game--computer pulse main__games--scissors  circle ">
+<img src="images/icon-scissors.svg" alt="icon-paper">
+<p>The House Picked</p>
+</div>`
+            ];
+            //  Random Image the computer gets
+            let rnd = Math.floor(Math.random() * computerImage.length);
 
-        //         //     // if (computerAnswer() === "http://127.0.0.1:5500/images/icon-paper.svg") {
-        //         //     //     console.log(computerAnswer())
-        //         //     // }
-        //         // }
-        //         console.log(humanAnswer());
-        //         console.log(computerAnswer());
-        //     }, 5000);
-
-        // }
-
+            // if (lastImagePicked === computerImage[rnd]) {
+            //     return randomImg();
+            // }
+            // lastImagePicked = computerImage[rnd];
+            return computerImage[rnd];
+        }
     }
 
+
+    // return {
+    //     computer: randomImg
+    // }
 })();
